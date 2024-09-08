41920e31ff08a132613c3d924ff5fe0d232cf11d
qtqz <qzqz7@qq.com>
2024-09-08 23:32:35 +0800

    新旧网站平滑迁移

--- valaxy-addon-other/App.vue
+++ valaxy-addon-other/App.vue
@@ -5,6 +5,7 @@
 <template>
   <TxSitePwd password="ciallo2"></TxSitePwd>
   <TxBdAnalyse src="https://hm.baidu.com/hm.js?30d8663ed5998a101dba65d5b1bdab74"></TxBdAnalyse>
+  <TxOldSwitch></TxOldSwitch>
 </template>
 
 <style lang="scss" scoped></style>
